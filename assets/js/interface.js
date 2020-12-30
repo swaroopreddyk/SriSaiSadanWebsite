abf545bed2a050c3469ef10d47758dbbab46ef6e
skondreddy <skondreddy@sescollc.com>
2020-12-29 19:00:43 -0500

    init commit

---
+++ assets/js/interface.js
@@ -0,0 +1,395 @@
+﻿( function($) {
+  'use strict';
+$(function(e) {
+
+/*-------------------------------------------------------------------------------
+	  Sticky-Header
+	-------------------------------------------------------------------------------*/
+$(window).scroll(function(){
+  var sticky = $('#header'),
+      scroll = $(window).scrollTop();
+
+  if (scroll >= 100) sticky.addClass('sticky');
+  else sticky.removeClass('sticky');
+});
+
+/*-------------------------------------------------------------------------------
+	  Mobile Navigation
+	-------------------------------------------------------------------------------*/
+
+    $('li.has-child > a').after('<div class="child-trigger"><i></i></div>');
+
+    $('#nav-toggle-label, .mobile-trigger').on('click', function() {
+      $('body').toggleClass('mobile-open');
+      $('.child-trigger').removeClass('child-open');
+      $('.mobile-menu .sub-menu').slideUp(250);
+      return false;
+    });
+
+    $('.mobile-trigger2').on('click', function() {
+      $('body').toggleClass('mobile-open-2');
+      $('.child-trigger').removeClass('child-open');
+      $('.mobile-menu .sub-menu').slideUp(250);
+      return false;
+    });
+
+    $('.child-trigger').on('click', function() {
+      var $parent = $(this).parent().siblings('.has-child');
+      $parent.find('.child-trigger').removeClass('child-open');
+      $parent.find('.sub-menu').slideUp(250);
+
+      $(this).next('.sub-menu').slideToggle(250);
+      $(this).toggleClass('child-open');
+      return false;
+    });
+
+    $('.mob-menu-overlay').on('click', function() {
+      $('body').removeClass('mobile-open');
+      $('body').removeClass('mobile-open-2');
+      $('.child-trigger').removeClass('child-open');
+      $('.mob-menu .sub-menu').slideUp(250);
+      return false;
+    });
+/*-------------------------------------------------------------------------------
+	  Video
+	-------------------------------------------------------------------------------*/
+$('.popup-youtube').magnificPopup({
+		type: 'iframe'
+	});
+	$('.popup-vimeo').magnificPopup({
+		type: 'iframe'
+	});
+	$('.popup-video').magnificPopup({
+		type: 'iframe'
+	});
+
+/*------------------------------------------------------------------
+	Intro-Slider
+	-------------------------------------------------------------------*/
+	$('#intro .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+	dots:false,
+	autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{items:1},
+        600:{items:1},
+        1000:{items:1}
+    }
+	});
+
+
+
+    /*------------------------------------------------------------------
+         Event Verticle-slick-Slider
+         -------------------------------------------------------------------*/
+
+     if ($('.event-slider').length) {
+       $('.event-slider').slick({
+         slidesToShow: 1,
+         autoplay: true,
+         autoplaySpeed: 2000,
+         speed: 2500,
+         dots: false,
+         arrows: true,
+         prevArrow: $('.tl-left-arrow'),
+         nextArrow: $('.tl-right-arrow'),
+         vertical: true,
+         responsive: [{
+             breakpoint: 1024,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+             }
+           },
+           {
+             breakpoint: 600,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+             }
+           },
+           {
+             breakpoint: 480,
+             settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+             }
+           }
+         ]
+       });
+     }
+
+
+     /*------------------------------------------------------------------
+          blog-owl-Slider
+          -------------------------------------------------------------------*/
+
+          $('#blog .owl-carousel').owlCarousel({
+            autoplay: true,
+            lazyLoad: true,
+            loop: true,
+            margin: 20,
+            dots:false,
+            arrows:true,
+            responsiveClass: true,
+            autoHeight: true,
+            autoplayTimeout: 3000,
+            smartSpeed: 800,
+            nav: true,
+            responsive: {
+              0: {
+                items: 1
+              },
+
+              600: {
+                items: 2
+              },
+
+              1024: {
+                items: 3
+              },
+
+              1366: {
+                items: 3
+              }
+            }
+
+        	});
+
+
+          /*------------------------------------------------------------------
+               About Page team-slider
+               -------------------------------------------------------------------*/
+
+               $('#about-team .owl-carousel').owlCarousel({
+                 autoplay: true,
+                 lazyLoad: true,
+                 loop: true,
+                 margin: 20,
+                 dots:false,
+                 arrows:true,
+                 responsiveClass: true,
+                 autoHeight: true,
+                 autoplayTimeout: 2000,
+                 smartSpeed: 800,
+                 nav: true,
+                 responsive: {
+                   0: {
+                     items: 1
+                   },
+
+                   600: {
+                     items: 2
+                   },
+
+                   1024: {
+                     items: 3
+                   },
+
+                   1366: {
+                     items: 4
+                   }
+                 }
+
+               });
+
+
+/*------------------------------------------------------------------
+	Causes-Slider
+	-------------------------------------------------------------------*/
+	$('#causes .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+	dots:false,
+	autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{items:1},
+        600:{items:1},
+        1000:{items:1}
+    }
+	});
+
+  /*------------------------------------------------------------------
+    	range-slider
+    	-------------------------------------------------------------------*/
+
+      $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        skin: "round",
+        hide_min_max: true,
+        min: 0,
+        max: 1000,
+        from: 1,
+        to: 1000,
+        grid: false,
+        prefix: "$"
+    });
+
+
+/*------------------------------------------------------------------
+	Testimonials-Slider
+	-------------------------------------------------------------------*/
+	$('#testimonials .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+	dots:true,
+	autoplay:true,
+    autoplayTimeout:4000,
+    responsive:{
+        0:{items:1},
+        600:{items:1},
+        1000:{items:1}
+    }
+	});
+	});
+
+/*------------------------------------------------------------------
+	back to top
+	-------------------------------------------------------------------*/
+ var top = $('#back-top');
+	top .hide();
+
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				top .fadeIn();
+			} else {
+				top .fadeOut();
+			}
+		});
+		$('#back-top a').on('click', function(e) {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+
+
+    //---------- countdown js--------------------//
+
+    // const second = 1000,
+    //       minute = second * 60,
+    //       hour = minute * 60,
+    //       day = hour * 24;
+    //
+    // let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+    //     x = setInterval(function() {
+    //
+    //       let now = new Date().getTime(),
+    //           distance = countDown - now;
+    //
+    //       document.getElementById('days').innerText = Math.floor(distance / (day)),
+    //         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+    //         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+    //         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+    //
+    //       //do something later when date is reached
+    //       //if (distance < 0) {
+    //       //  clearInterval(x);
+    //       //  'IT'S MY BIRTHDAY!;
+    //       //}
+    //
+    //     }, second)
+
+
+/*------------------------------------------------------------------
+	Countdown
+	-------------------------------------------------------------------*/
+  // 2019[year] - 8[month] - 20[day]
+
+  $('#countdown').countdown('2020/05/20', function(event) {
+	$(this).html(event.strftime('<span class="countdown-period">%-D <span>Day%!D</span></span> <span class="countdown-period">%H <span>Hours</span></span> <span class="countdown-period">%M <span>Minutes</span></span> <span class="countdown-period">%S <span>Seconds</span></span>'));
+  });
+
+  // color switch
+  $( ".each-color.co1" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co2" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co3" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co4" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co5" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co6" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co7" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co8" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co9" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co10" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+  $( ".each-color.co11" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+  $( ".each-color.co12" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+
+  $( ".each-color.co13" ).on('click', function(e) {
+      var data1 = $(this).data('color');
+      $(".color-changing").attr('href', data1);
+  });
+// Font Switecher
+  $('#fonts').bind('change', function() {
+  		var font = $(this).val();
+  		$('h1, h2, h3, h4, h5').css('fontFamily', font);
+  	});
+    // Font Switecher
+      $('#bodyfonts').bind('change', function() {
+      		var font = $(this).val();
+      		$('body, p').css('fontFamily', font);
+      	});
+      $(document).ready(function(){
+      	$(".theme-switch").click(function(){
+      		$(".color-theme").toggleClass("active");
+      	});
+      });
+      $(document).ready(function(){
+        $(".screen-switch").click(function(){
+          $(".themes-screens").toggleClass("active");
+        });
+      });
+      
+
+
+})(jQuery);
---
+++ player.bc.cdn.bitgravity.com/10/functions.js
@@ -0,0 +1,27 @@
+// Declaration of arrays for OSMF Player setup
+var flashvars = {};
+var htmlvars = {};
+var params = {};
+var attributes = {};
+var info = {};
+
+info.BitGravityswf = "http://player.bc.cdn.bitgravity.com/10/BitGravityPlayer.swf";
+//info.swfVersionStr = "11.2.202.235";
+info.swfVersionStr = "10.3.0";
+info.xiSwfUrlStr = "http://player.bc.cdn.bitgravity.com/10/playerProductInstall.swf";
+
+// Initalization of params for player configuration etc.
+params.quality = "high";
+params.bgcolor = "#000000";
+params.allowscriptaccess = "always";
+params.allowfullscreen = "true";
+
+// Initialization of attributes for player description etc.
+attributes.id = "BitGravityPlayer";
+attributes.name = "BitGravityPlayer";
+attributes.align = "left";
+
+htmlvars.playbackdivId = "strobeMediaPlaybackId";
+
+
+
